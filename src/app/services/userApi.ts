41750e1c397eacac7e845a8cc83d7f37659d5277
Anastasia <anastasiaerofeeva121@gmail.com>
2025-10-16 19:38:47 +0300

    user api

---
+++ src/app/services/userApi.ts
@@ -0,0 +1,48 @@
+import type { User } from "../../types/type";
+import { api } from "./api";
+
+export const userApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation<{token:string}, {email:string, password:string}> ({
+      query: (userData) => ({
+        url:'/login',
+        method:'POST',
+        body:userData
+      })
+    }),
+
+    register: builder.mutation<{email:string, password:string, name:string}, {email:string, password:string, name:string}> ({
+      query:(userData) => ({
+        url:'/register',
+        method:'POST',
+        body:userData
+      })
+    }),
+
+    getUserById: builder.query<User, string> ({
+      query:(userId) => ({
+        url:`/users/${userId}`,
+        method:'GET'
+      })
+    }),
+
+    current: builder.query<User, void> ({
+      query: () => ({
+        url:'/current',
+        method:'GET',
+      })
+    }),
+
+    updateUser: builder.mutation<User,{userData: FormData, id:string}> ({
+      query: ({userData, id}) => ({
+        method: 'PUT',
+        url:`/users/${id}`,
+        body:userData
+      })
+    })
+  })
+})
+
+export const {
+  
+} = userApi
